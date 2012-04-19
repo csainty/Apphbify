422a3e49a4878663ec2c80c98815b3c24eb61d68
Chris Sainty <csainty@hotmail.com>
2012-04-19 18:55:09 +1000

    Initial commit of basic nancy project.

---
+++ Apphbify/PagesModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace Apphbify
+{
+    public class PagesModule : NancyModule
+    {
+        public PagesModule()
+        {
+            Get["/"] = Home;
+        }
+
+        private Response Home(dynamic parameters)
+        {
+            return View["Home"];
+        }
+    }
+}
