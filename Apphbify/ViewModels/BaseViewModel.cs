f8f8fe6cce0e1e904defd72e921d074c0bb331c0
Chris Sainty <csainty@hotmail.com>
2012-04-22 18:58:14 +1000

    Added google analytics

--- Apphbify/ViewModels/BaseViewModel.cs
+++ Apphbify/ViewModels/BaseViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using Nancy.Session;
 
 namespace Apphbify.ViewModels
@@ -6,11 +7,18 @@ namespace Apphbify.ViewModels
     public class BaseViewModel
     {
         public bool IsLoggedOn { get; set; }
+
         public string ErrorFlash { get; set; }
+
         public string SuccessFlash { get; set; }
+
         public string CurrentPage { get; set; }
+
         public bool HasErrorFlash { get { return !String.IsNullOrWhiteSpace(ErrorFlash); } }
+
         public bool HasSuccessFlash { get { return !String.IsNullOrWhiteSpace(SuccessFlash); } }
+
+        public string GoogleAnalyticsKey { get; set; }
 
         public BaseViewModel(string pageName, ISession session)
         {
@@ -18,6 +26,7 @@ namespace Apphbify.ViewModels
             IsLoggedOn = session[SessionKeys.ACCESS_TOKEN] != null;
             ErrorFlash = session[SessionKeys.FLASH_ERROR] as string;
             SuccessFlash = session[SessionKeys.FLASH_SUCCESS] as string;
+            GoogleAnalyticsKey = ConfigurationManager.AppSettings["GA_KEY"];
             session.Delete(SessionKeys.FLASH_ERROR);
             session.Delete(SessionKeys.FLASH_SUCCESS);
         }
