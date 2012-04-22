803edfa41b9cd2430bf77777108f1ef8ea530d03
Chris Sainty <csainty@hotmail.com>
2012-04-22 13:12:36 +1000

    Added /Sites page to list off the sites you currently have hosted on AppHarbor.

---
+++ Apphbify/PagesModule.Secure.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AppHarbor;
+using Apphbify.ViewModels;
+using Nancy;
+
+namespace Apphbify
+{
+    public class SecuredPagesModule : NancyModule
+    {
+        private AppHarborApi _Api;
+
+        public SecuredPagesModule()
+        {
+            Before.AddItemToEndOfPipeline(CheckAuth);
+
+            Get["/Sites"] = Sites;
+        }
+
+        private Response Sites(dynamic parameters)
+        {
+            return View["Sites", new SitesViewModel(_Api, Request.Session)];
+        }
+
+        private Response CheckAuth(NancyContext ctx)
+        {
+            var token = ctx.Request.Session[SessionKeys.ACCESS_TOKEN] as string;
+            if (String.IsNullOrEmpty(token))
+                return Response.AsRedirect("/SignIn");
+            _Api = new AppHarborApi(new AuthInfo { AccessToken = token, TokenType = "Bearer" });
+            return null;
+        }
+    }
+}
---
+++ Apphbify/ViewModels/SitesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using AppHarbor;
+using AppHarbor.Model;
+using Nancy.Session;
+
+namespace Apphbify.ViewModels
+{
+    public class SitesViewModel : BaseViewModel
+    {
+        public IList<Application> Sites { get; set; }
+
+        public SitesViewModel(AppHarborApi api, ISession session)
+            : base("Sites", session)
+        {
+            Sites = api.GetApplications();
+        }
+    }
+}
