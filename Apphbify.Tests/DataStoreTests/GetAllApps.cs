9989e44d5ff68cb1bb871fe37ddda26891246afa
Chris Sainty <csainty@hotmail.com>
2012-05-05 16:57:50 +1000

    Added a test project

---
+++ Apphbify.Tests/DataStoreTests/GetAllApps.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Apphbify.Data;
+using Xunit;
+
+namespace Apphbify.Tests.DataStoreTests
+{
+    public class GetAllApps
+    {
+        IList<App> _Apps;
+
+        public GetAllApps()
+        {
+            _Apps = new DataStore().GetAllApps();
+        }
+
+        [Fact]
+        public void Should_contain_entries()
+        {
+            Assert.NotEqual(0, _Apps.Count);
+        }
+    }
+}
