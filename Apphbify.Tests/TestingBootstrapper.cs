fc55122b968f151b25333f79668e03fff0b31889
Chris Sainty <csainty@hotmail.com>
2012-05-05 17:27:26 +1000

    Added support for mocking.

--- Apphbify.Tests/TestingBootstrapper.cs
+++ Apphbify.Tests/TestingBootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Apphbify.Services;
+using Moq;
 using Nancy;
 using TinyIoC;
 
@@ -6,9 +7,24 @@ namespace Apphbify.Tests
 {
     public class TestingBootstrapper : DefaultNancyBootstrapper
     {
+        private readonly Mock<IOAuth> _OAuth;
+
+        public TestingBootstrapper(Mock<IOAuth> oauth = null)
+        {
+            _OAuth = oauth ?? DefaultIOAuth();
+        }
+
         protected override void ConfigureRequestContainer(TinyIoCContainer container, NancyContext context)
         {
-            container.Register<IOAuth>(new OAuth("", "", ""));
+            container.Register<IOAuth>(_OAuth.Object);
+        }
+
+        private Mock<IOAuth> DefaultIOAuth()
+        {
+            var mock = new Mock<IOAuth>();
+            mock.Setup(d => d.GetAuthUrl()).Returns("http://auth.com");
+            mock.Setup(d => d.GetAccessToken(It.IsAny<string>())).Returns("12345");
+            return mock;
         }
     }
 }
