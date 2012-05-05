c07a2829667ee3ce7d3ac2f0df654c5cc42b8f23
Chris Sainty <csainty@hotmail.com>
2012-05-05 18:53:52 +1000

    Added tests for /About and /Apps

---
+++ Apphbify.Tests/Pages/Apps/When_displaying_the_apps_page.cs
@@ -0,0 +1,47 @@
+﻿using Nancy;
+using Nancy.Testing;
+using Xunit;
+
+namespace Apphbify.Tests.Pages.Apps
+{
+    public class When_displaying_the_apps_page
+    {
+        private Browser _Browser;
+        private BrowserResponse _Response;
+
+        public When_displaying_the_apps_page()
+        {
+            _Browser = new Browser(new TestingBootstrapper());
+            _Response = _Browser.Get("/Apps");
+        }
+
+        [Fact]
+        public void It_should_return_200()
+        {
+            Assert.Equal(HttpStatusCode.OK, _Response.StatusCode);
+        }
+
+        [Fact]
+        public void It_should_render_the_master_page()
+        {
+            _Response.Body["a.brand"]
+                .ShouldExist()
+                .And.ShouldContain("AppHarbify!");
+        }
+
+        [Fact]
+        public void It_should_render_the_view_content()
+        {
+            _Response.Body[".page-header h1"]
+                .ShouldExist()
+                .And.ShouldContain("Apps");
+        }
+
+        [Fact]
+        public void It_should_render_table_rows()
+        {
+            _Response.Body["tr.app"]
+                .ShouldExist();
+        }
+    }
+}
