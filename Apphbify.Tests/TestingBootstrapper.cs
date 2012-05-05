cfb15f6df913ee6ddba906bfac7f1123e1ac2c30
Chris Sainty <csainty@hotmail.com>
2012-05-05 18:25:52 +1000

    Added tests for the home page

--- Apphbify.Tests/TestingBootstrapper.cs
+++ Apphbify.Tests/TestingBootstrapper.cs
@@ -1,52 +1,41 @@
-﻿using Apphbify.Services;
+﻿using System.Collections.Generic;
+using Apphbify.Services;
+using Apphbify.Tests.Helpers;
 using Moq;
 using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Testing;
+using Nancy.Testing.Fakes;
 using TinyIoC;
 
 namespace Apphbify.Tests
 {
-    public class TestingBootstrapper : DefaultNancyBootstrapper
+    public class TestingBootstrapper : ConfigurableBootstrapper
     {
         private readonly Mock<IApiService> _Api;
         private readonly Mock<IDeploymentService> _Deploy;
         private readonly Mock<IOAuth> _OAuth;
         private readonly Mock<IMailService> _Mail;
+        private readonly Dictionary<string, object> _SessionData;
 
-        public TestingBootstrapper(Mock<IApiService> api = null, Mock<IDeploymentService> deploy = null, Mock<IOAuth> oauth = null, Mock<IMailService> mail = null)
+        public TestingBootstrapper(Mock<IApiService> api = null, Mock<IDeploymentService> deploy = null, Mock<IOAuth> oauth = null, Mock<IMailService> mail = null, Dictionary<string, object> sessionData = null)
         {
-            _Api = api ?? DefaultIApi();
-            _Deploy = deploy ?? DefaultIDeploy();
-            _OAuth = oauth ?? DefaultIOAuth();
-            _Mail = mail ?? DefaultIMail();
+            FakeRootPathProvider.RootPath = "../../../Apphbify";    // Repoint the root path so we can find views
+            _Api = api ?? new Mock<IApiService>(MockBehavior.Strict);
+            _Deploy = deploy ?? new Mock<IDeploymentService>(MockBehavior.Strict);
+            _OAuth = oauth ?? new Mock<IOAuth>(MockBehavior.Strict);
+            _Mail = mail ?? new Mock<IMailService>(MockBehavior.Strict);
+            _SessionData = sessionData;
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            TestingSession.Enable(pipelines, _SessionData);
         }
 
         protected override void ConfigureRequestContainer(TinyIoCContainer container, NancyContext context)
         {
             container.Register<IOAuth>(_OAuth.Object);
-        }
-
-        private Mock<IOAuth> DefaultIOAuth()
-        {
-            var mock = new Mock<IOAuth>(MockBehavior.Strict);
-            return mock;
-        }
-
-        private Mock<IApiService> DefaultIApi()
-        {
-            var mock = new Mock<IApiService>(MockBehavior.Strict);
-            return mock;
-        }
-
-        private Mock<IDeploymentService> DefaultIDeploy()
-        {
-            var mock = new Mock<IDeploymentService>(MockBehavior.Strict);
-            return mock;
-        }
-
-        private Mock<IMailService> DefaultIMail()
-        {
-            var mock = new Mock<IMailService>(MockBehavior.Strict);
-            return mock;
         }
     }
 }
