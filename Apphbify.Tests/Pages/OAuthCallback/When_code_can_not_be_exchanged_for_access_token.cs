00c63cc89c1ecc12bb08b6ae765718beaf5a2d99
Chris Sainty <csainty@hotmail.com>
2012-05-10 10:51:44 +1000

    Added tests for /callback

---
+++ Apphbify.Tests/Pages/OAuthCallback/When_code_can_not_be_exchanged_for_access_token.cs
@@ -0,0 +1,47 @@
+﻿using Apphbify.Services;
+using Apphbify.Tests.Helpers;
+using Moq;
+using Nancy.Testing;
+using Xunit;
+
+namespace Apphbify.Tests.Pages.OAuthCallback
+{
+    public class When_code_can_not_be_exchanged_for_access_token
+    {
+        private Browser _Browser;
+        private BrowserResponse _Response;
+        private Mock<IOAuth> _OAuth;
+
+        public When_code_can_not_be_exchanged_for_access_token()
+        {
+            _OAuth = new Mock<IOAuth>(MockBehavior.Strict);
+            _OAuth.Setup(d => d.GetAccessToken(It.IsAny<string>())).Returns((string)null);
+            _Browser = Testing.CreateBrowser<OAuthModule>(with =>
+            {
+                with.OAuth(_OAuth);
+            });
+            _Response = _Browser.Get("/callback", with =>
+            {
+                with.Query("code", "testcode");
+            });
+        }
+
+        [Fact]
+        public void It_should_try_get_token()
+        {
+            _OAuth.Verify(d => d.GetAccessToken("testcode"), Times.Once());
+        }
+
+        [Fact]
+        public void It_should_redirect_to_home()
+        {
+            _Response.ShouldHaveRedirectedTo("/");
+        }
+
+        [Fact]
+        public void It_should_have_error_message()
+        {
+            _Response.ShouldHaveErrorMessage();
+        }
+    }
+}
---
+++ Apphbify.Tests/Pages/OAuthCallback/When_oauth_callback_is_received.cs
@@ -0,0 +1,53 @@
+﻿using Apphbify.Services;
+using Apphbify.Tests.Helpers;
+using Moq;
+using Nancy.Testing;
+using Xunit;
+
+namespace Apphbify.Tests.Pages.OAuthCallback
+{
+    public class When_oauth_callback_is_received
+    {
+        private Browser _Browser;
+        private BrowserResponse _Response;
+        private Mock<IOAuth> _OAuth;
+
+        public When_oauth_callback_is_received()
+        {
+            _OAuth = new Mock<IOAuth>(MockBehavior.Strict);
+            _OAuth.Setup(d => d.GetAccessToken(It.IsAny<string>())).Returns("12345");
+            _Browser = Testing.CreateBrowser<OAuthModule>(with =>
+            {
+                with.OAuth(_OAuth);
+            });
+            _Response = _Browser.Get("/callback", with =>
+            {
+                with.Query("code", "testcode");
+            });
+        }
+
+        [Fact]
+        public void It_should_exchange_the_code()
+        {
+            _OAuth.Verify(d => d.GetAccessToken("testcode"), Times.Once());
+        }
+
+        [Fact]
+        public void It_should_save_the_access_token()
+        {
+            Assert.Equal("12345", _Response.Context.Request.Session[SessionKeys.ACCESS_TOKEN]);
+        }
+
+        [Fact]
+        public void It_should_redirect_to_apps()
+        {
+            _Response.ShouldHaveRedirectedTo("/Apps");
+        }
+
+        [Fact]
+        public void It_should_have_a_success_message()
+        {
+            _Response.ShouldHaveSuccessMessage();
+        }
+    }
+}
