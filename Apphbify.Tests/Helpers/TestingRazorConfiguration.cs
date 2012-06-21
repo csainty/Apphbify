731acfdb0429925a7ba30653f3cdecf3dd467282
Chris Sainty <csainty@hotmail.com>
2012-06-21 22:07:47 +1000

    Be explicit about System reference in unit tests as well. Previous code worked in NCrunch but fails in TestDriven and on AppHarbor.

--- Apphbify.Tests/Helpers/TestingRazorConfiguration.cs
+++ Apphbify.Tests/Helpers/TestingRazorConfiguration.cs
@@ -12,7 +12,7 @@ namespace Apphbify.Tests.Helpers
 
         public IEnumerable<string> GetAssemblyNames()
         {
-            return new[] { "System" };
+            return new[] { "System, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" };
         }
 
         public IEnumerable<string> GetDefaultNamespaces()
